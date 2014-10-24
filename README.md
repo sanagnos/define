8b791f83cc7f3911fac6054b3d67dab41261eba9
Stelios <stelios@outlook.com>
2014-10-23 21:11:30 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,76 @@
+define
+======
+
+> Prototypal class & interface inheritance in Javascript
+
+The scope of this is to formalize class definitions with shared interfaces & superclasses, useful for ensuring x-module compatibility & DRYness.
+
+The approach is inline with Javascript's orthogonal take on type-checking.
+
+## Getting started
+```javascript
+// ============================================================================
+// Interface contract definition
+// ============================================================================
+
+var IFeline = {
+
+    sleep: function () {
+        return 'prepare for tomorrow';
+    },
+
+    eat: function () {
+        return 'beware of poison';
+    },
+
+    pet: function () {
+        if (this.habbits && this.habbits.couchSleeper)
+            return Math.random() > 0.5 ? 'bite' : 'survive';
+        return 'bite';
+    },
+
+    walk: function () {
+        return 'escape';
+    }
+};
+
+var IPet = {
+    isSleepy        : 'sleepy',
+    sleepsOnTheCouch: 'habbits.couchSleeper',
+    calcHappiness: function (ate) {
+        return ate && this.isSleepy ? 0.9 : -10;
+    }
+};
+
+// ============================================================================
+// Class implementation definition
+// ============================================================================
+
+var Cat = define(function (color, sleepy, habbits) {
+    this.color   = color;
+    this.sleepy  = sleepy;
+    this.habbits = habbits;
+}, [
+    IFeline,
+    IPet
+]);
+
+// ============================================================================
+// Main
+// ============================================================================
+
+var overTheCouchCat  = new Cat('black', true,  { couchSleeper: true  }),
+    underTheCouchCat = new Cat('brown', false, { couchSleeper: false });
+
+console.log([
+    overTheCouchCat.pet(),                // survive
+    overTheCouchCat.isSleepy,             // true
+    overTheCouchCat.sleepsOnTheCouch,     // true
+    overTheCouchCat.calcHappiness(true),  // 0.9
+
+    underTheCouchCat.pet(),               // bite
+    underTheCouchCat.isSleepy,            // false
+    underTheCouchCat.sleepsOnTheCouch,    // false
+    overTheCouchCat.calcHappiness(false), // -10
+].join('\n'));
+```
---
+++ define.js
@@ -0,0 +1,123 @@
+// ============================================================================
+// define.js
+// ============================================================================
+// 
+// Prototypal class & interface inheritance in Javascript.
+// 
+// @authors
+//      10/23/14    Stelios Anagnostopoulos     stelios@outlook.com
+//      
+// Copyright 2014, the @authors. All rights reserved.
+// ============================================================================
+
+typeof window === 'undefined' ? module.exports : window.define = (function () {
+
+    'use strict';
+
+    // ========================================================================
+    // Definition
+    // ========================================================================
+
+    /**
+     * Generates class definition given a collection of prototype
+     * contracts.
+     * 
+     * @param  {Function} constr    
+     * @param  {Array}    contracts (can also be single object)
+     * @return {Function}          
+     */
+    function define (constr, contracts) {
+
+        if (!(contracts instanceof Array))
+            contracts = [contracts];
+        for (var i = 0, len = contracts.length; i < len; i++)
+            bind(constr.prototype, contracts[i]);
+
+        var init = function () {
+            if (constr.prototype._preInit)
+                for (var i = 0, len = constr.prototype._preInit.length; i < len; i++)
+                    constr.prototype._preInit[i].apply(this, arguments);
+
+            constr.apply(this, arguments);
+
+            if (constr.prototype._postInit)
+                for (var i = 0, len = constr.prototype._postInit.length; i < len; i++)
+                    constr.prototype._postInit[i].apply(this, arguments);
+        };
+
+        init.prototype = new constr;
+        init.prototype.constructor = init;
+
+        return init;
+    };
+
+    // ========================================================================
+    // Internal
+    // ========================================================================
+    
+    // cache
+    var defineProperties = Object.defineProperties;
+
+    /**
+     * Binds members to class definition.
+     * 
+     * @param  {Object} proto
+     * @param  {Object} attr 
+     * @return {Object}      
+     */
+    function bind (proto, attr) {
+        var properties = {},
+            methods    = [],
+            tkey;
+        for (var key in attr) {
+            if ( typeof attr[key] === 'string' ) {
+                properties[key] = ( function (proparr) {
+                    return {
+                        configurable: true,
+                        get: function() {
+                            var ctx = this[proparr[0] ];
+                            for (var i = 1, len = proparr.length; i < len; i++)
+                                ctx = ctx[proparr[i]];
+                            return ctx;
+                        },
+                        set: function(v) {
+                            var ctx = this[ proparr[0] ];
+                            for (var i = 1, len = proparr.length - 1; i < len; i++)
+                                ctx = ctx[ proparr[i] ];
+                            ctx[ proparr[ proparr.length - 1 ] ] = v;
+                        },
+                    }
+                })( attr[key].split('.') );
+            } else if ( typeof attr[key] === 'object' ) {
+                properties[key] = attr[key];
+            } else if ( typeof attr[key] === 'function' ) {
+                if ( key === 'preInit' || key === 'postInit' ) {
+                    tkey = '_' + key;
+                    if ( !proto[tkey] )
+                        proto[tkey] = [];
+                    proto[ tkey ][ proto[tkey].length ] = attr[key];   
+                } else {
+                    methods[ methods.length ] = [ key, attr[key] ];
+                }
+            }
+        }
+
+        defineProperties(proto, properties);
+
+        var key, fun, sup;
+        for (var i = 0, len = methods.length; i < len; i++) {
+            key = methods[i][0];
+            fun = methods[i][1];
+            if (proto[key])
+                sup = proto[key];
+            proto[key]       = fun;
+            proto[key].super = sup;
+        }
+    };
+
+    // ========================================================================
+    // Exports
+    // ========================================================================
+
+    return define;
+})();
